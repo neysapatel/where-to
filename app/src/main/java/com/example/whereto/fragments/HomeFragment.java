af498c3f929a27dc5275b3e549df465006c54f84
neysapatel <patelneysa@gmail.com>
2022-06-30 09:03:04 -0700

    The app can also get events from the Yelp API now!

--- app/src/main/java/com/example/whereto/fragments/HomeFragment.java
+++ app/src/main/java/com/example/whereto/fragments/HomeFragment.java
@@ -5,12 +5,61 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.whereto.Constants;
 import com.example.whereto.R;
+import com.example.whereto.models.YelpFusion;
+import com.example.whereto.models.YelpService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class HomeFragment extends Fragment {
     public HomeFragment() {
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(Constants.BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+        YelpService yelpService = retrofit.create(YelpService.class);
+
+        String searchTerm = "Avocado Toast"; // TODO: replace with user input
+        String searchCategory = "Concert"; // TODO: replace with user input
+        String location = "San Francisco"; // TODO: replace with user input
+
+        Call<YelpFusion> restaturantCall = yelpService.searchRestaurants("Bearer "+ Constants.API_KEY, searchTerm, location);
+        restaturantCall.enqueue(new Callback<YelpFusion>() {
+            @Override
+            public void onResponse(Call<YelpFusion> call, Response<YelpFusion> response) {
+                // TODO: store and return response based on the user's preferences
+            }
+
+            @Override
+            public void onFailure(Call<YelpFusion> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
+
+        Call<YelpFusion> eventsCall = yelpService.searchEvents("Bearer "+ Constants.API_KEY, searchCategory, location);
+        eventsCall.enqueue(new Callback<YelpFusion>() {
+            @Override
+            public void onResponse(Call<YelpFusion> eventsCall, Response<YelpFusion> response) {
+                // TODO: store and return response based on the user's preferences
+            }
+
+            @Override
+            public void onFailure(Call<YelpFusion> eventsCall, Throwable t) {
+                t.printStackTrace();
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/whereto/models/YelpService.java
+++ app/src/main/java/com/example/whereto/models/YelpService.java
@@ -8,4 +8,5 @@ import retrofit2.http.Query;
 public interface YelpService {
         @GET("businesses/search")
         Call<YelpFusion> searchRestaurants(@Header("Authorization") String authHeader, @Query("term") String searchTerm, @Query("location") String location);
+        Call<YelpFusion> searchEvents(@Header("Authorization") String authHeader, @Query("category") String searchTerm, @Query("location") String location);
     }
